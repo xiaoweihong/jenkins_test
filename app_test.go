180398e5de6d587772fc128ecf667261550115a6
xiaowei <403828237@qq.com>
2019-01-22 20:04:11 +0800

    修改jenkins文件

--- app_test.go
+++ app_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func Test_GetNameLen_1(t *testing.T) {
-	if l := getNameLen("nick"); l != 4 {
+	if l := getNameLen("nick2"); l != 4 {
 		t.Error("test failed, the length of nick is not correct.")
 	} else {
 		t.Log("test passed.")
