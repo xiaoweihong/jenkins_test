9999d9ad39989c54da126a61a6409c81b8b0eb54
xiaowei <403828237@qq.com>
2019-01-22 16:54:08 +0800

    qq

--- buildapp.sh
+++ buildapp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -ex
-docker build -t gowebdemo jenkins/Dockerfile
+docker build -t gowebdemo .
 
 # remove all none tag images
 if [ ! -z "$(docker images -q --filter 'dangling=true')" ]; then
